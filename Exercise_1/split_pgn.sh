1189d78fdea25ddebb83732b5da07bd72e284163
Gilad Carmel <giladcarmel22@gmail.com>
2025-03-28 11:57:38 +0300

    refactor to use functions

--- Exercise_1/split_pgn.sh
+++ Exercise_1/split_pgn.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-# number of arguemnts should be 2
-if [ $# -ne 2 ]
-then
-  echo "Usage: $0 <source_pgn_file> <destination_directory>"
-  exit 1
-fi
+# number of arguments should be 2
+validate_arguments() {
+    if [ $# -ne 2 ]; then
+        echo "Usage: $0 <source_pgn_file> <destination_directory>"
+        exit 1
+    fi
+}
 
-# parse argument names to normal names
+# check if the input file exists
+check_input_file() {
+    local input_file=$1
+    if [ ! -f "$input_file" ]; then
+        echo "Error: File '$input_file' does not exist."
+        exit 1
+    fi
+}
+
+# create destination directory
+ensure_directory_exists() {
+    local dest_dir=$1
+    if [ ! -d "$dest_dir" ]; then
+        mkdir "$dest_dir"
+        echo "Created directory '$dest_dir'."
+    fi
+}
+
+# Main script logic
+validate_arguments "$@"
 INPUT_FILE=$1
 DEST_DIR=$2
-
-# check if input file exists
-if [ ! -f "$INPUT_FILE" ]
-then
-    echo "Error: File '$INPUT_FILE' does not exist."
-    exit 1
-fi
-
-# check if dir exists and if not create it
-if [ ! -d "$DEST_DIR" ]
-then
-    mkdir "$DEST_DIR"
-    echo "Created directory '$DEST_DIR'."
-    exit 1
-fi
+check_input_file "$INPUT_FILE"
+ensure_directory_exists "$DEST_DIR"
