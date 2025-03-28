6af13043df7035cb837c3f142e7b26f033cc2ad3
Gilad Carmel <giladcarmel22@gmail.com>
2025-03-28 11:44:02 +0300

    input file exists check

--- Exercise_1/split_pgn.sh
+++ Exercise_1/split_pgn.sh
@@ -6,3 +6,14 @@ then
   echo "Usage: $0 <source_pgn_file> <destination_directory>"
   exit 1
 fi
+
+# parse argument names to normal names
+INPUT_FILE=$1
+OUTPUT_FILE=$2
+
+# check if input file exists
+if [ ! -f "$INPUT_FILE" ]
+then
+    echo "Error: File '$INPUT_FILE' does not exist."
+    exit 1
+fi
